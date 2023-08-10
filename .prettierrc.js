0748b93d860840b6c99939616d57d2cfa6ab01f2
Sunil Pai <threepointone@gmail.com>
2023-08-11 00:01:00 +0100

    update packages

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,11 +5,7 @@ module.exports = {
   singleQuote: false,
   tabWidth: 2,
   useTabs: false,
-  plugins: [
-    require("prettier-plugin-astro"),
-    require("prettier-plugin-tailwindcss") /* Must come last */,
-  ],
-  pluginSearchDirs: false,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss" /* Must come last */],
   overrides: [
     {
       files: "**/*astro",
