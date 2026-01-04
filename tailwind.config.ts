60a81438701fae5fffb89aba4b5fa458b3e2491b
Sunil Pai <spai@cloudflare.com>
2026-01-04 16:58:23 +0000

    Remove ESLint config and update editor settings
    Deleted ESLint configuration files and related settings from .vscode/settings.json to remove linting integration. Also removed .npmrc and a public image. Cleaned up Tailwind config comments.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -34,9 +34,7 @@ export default {
       transitionProperty: {
         height: "height",
       },
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      // Remove above once tailwindcss exposes theme type
       typography: (theme) => ({
         cactus: {
           css: {
